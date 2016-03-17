6151bcc7d5c52224fa5f739fbdf1bcaaf14b4313
zhoukang <zhoukang@hoolai.com>
2016-03-17 14:30:45 +0800

    init

---
+++ ZK3DPop/ZK3DPop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZK3DPop
+//
+//  Created by ZK on 16/3/17.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZK3DPop/ZK3DPop/Classes/ZKMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKMainViewController.h
+//  ZK3DPop
+//
+//  Created by ZK on 16/3/17.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKMainViewController : UIViewController
+
+@end
